f2b3117282421840ade66baa7994e127802d2e81
LucasiBai <lucasibaigorria@mi.unc.edu.ar>
2023-06-22 22:49:52 -0300

    agregado de tests para email service + tests pasados

---
+++ apps/customer_messages/tests/test_email_service.py
@@ -0,0 +1,80 @@
+from django.test import TestCase
+
+from apps.customer_messages.utils.services.email_service import EmailService
+
+
+class EmailServiceTests(TestCase):
+    """
+    Tests Email Service
+    """
+
+    def setUp(self):
+        self.service = EmailService
+
+    def test_service_send_email_method_successful(self):
+        """
+        Tests if service can send email correctly
+        """
+        payload = {
+            "subject": "Test Subject",
+            "message": "Test Message",
+            "full_name_from": "Test Name",
+            "email_from": "testemail@test.com",
+            "recipient_email": "recipientemail@test.com"
+        }
+
+        self.assertTrue(self.service.send_email_to(**payload))
+
+    def test_service_send_email_no_param_reject(self):
+        """
+        Tests if service raise an error when don't have required param
+        """
+        payload = {
+            "message": "Test Message",
+            "full_name_from": "Test Name",
+            "email_from": "testemail@test.com",
+            "recipient_email": "recipientemail@test.com"
+        }
+
+        with self.assertRaises(ValueError):
+            self.service.send_email_to(**payload)
+
+        payload = {
+            "subject": "Test Subject",
+            "full_name_from": "Test Name",
+            "email_from": "testemail@test.com",
+            "recipient_email": "recipientemail@test.com"
+        }
+
+        with self.assertRaises(ValueError):
+            self.service.send_email_to(**payload)
+
+        payload = {
+            "subject": "Test Subject",
+            "message": "Test Message",
+            "email_from": "testemail@test.com",
+            "recipient_email": "recipientemail@test.com"
+        }
+
+        with self.assertRaises(ValueError):
+            self.service.send_email_to(**payload)
+
+        payload = {
+            "subject": "Test Subject",
+            "message": "Test Message",
+            "full_name_from": "Test Name",
+            "recipient_email": "recipientemail@test.com"
+        }
+
+        with self.assertRaises(ValueError):
+            self.service.send_email_to(**payload)
+
+        payload = {
+            "subject": "Test Subject",
+            "message": "Test Message",
+            "full_name_from": "Test Name",
+            "email_from": "testemail@test.com",
+        }
+
+        with self.assertRaises(ValueError):
+            self.service.send_email_to(**payload)
--- apps/customer_messages/utils/services/email_service.py
+++ apps/customer_messages/utils/services/email_service.py
@@ -10,4 +10,14 @@ class EmailService:
         Returns:
             True if email was sent
         """
+        subject = kwargs.get("subject", None)
+        message = kwargs.get("message", None)
+        full_name_from = kwargs.get("full_name_from", None)
+        email_from = kwargs.get("email_from", None)
+        recipient_email = kwargs.get("recipient_email", None)
+
+        if not (subject and message and full_name_from and email_from and recipient_email):
+            raise ValueError(
+                "send_email method must contain subject, message, full_name_from, email_from and recipient_email.")
+
         return True
